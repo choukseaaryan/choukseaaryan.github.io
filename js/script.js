1819e3c4cab7127166ff6433789626eca510bc1b
choukseaaryan <aaryan2chouksey@gmail.com>
2023-12-29 10:51:15 +0530

    changes in skills carousel

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ let value = 0;
 // trail index number
 let trailValue = 0;
 // interval (Duration)
-let interval = 4000;
+let interval = 4000000;
 
 // Function to slide forward
 const slide = (condition) => {
